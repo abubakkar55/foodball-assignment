8b4140687f1e21dd8895a3172e9e01a8c59641b6
Ab <computerm246@gmail.com>
2021-07-14 21:50:06 +0600

    done

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 
 const header  = document.querySelector('.header');
 const burgerBtn  = document.querySelector('.header__hamburger');
+const navLink  = document.querySelectorAll('.navbar__link');
 burgerBtn.addEventListener('click', function () {
     header.classList.toggle('open');
 })
+
